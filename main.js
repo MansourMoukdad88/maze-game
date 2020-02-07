ebbbedaebe1990479df2172e649696bb590f87b3
MansourMoukdad88 <m_sy@outlook.com>
2020-02-07 17:28:00 +0200

    Generate Random Shapes

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
 
 const engine = Engine.create();
 const {world} = engine;
+
+const width = 800;
+const height = 600;
+
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    wireframes:false,
+    width,
+    height
   }
 });
 Render.run(render);
@@ -25,4 +30,14 @@ const walls = [
   Bodies.rectangle(800, 300, 40, 600, {isStatic: true})
 ];
 World.add(world, walls);
-World.add(world, Bodies.rectangle(200,200,50,50));
+
+// Random Shapes
+
+for(let i = 0; i < 40; i++) {
+  if(Math.random() > 0.5) {
+    World.add(world, Bodies.rectangle(Math.random() * width,Math.random() * height,50,50));
+  } else {
+    World.add(world, Bodies.circle(Math.random() * width,Math.random() * height,35));
+  }
+}
+

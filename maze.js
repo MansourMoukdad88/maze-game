20168a11495234c1538a7873b8ae5483f047f41f
MansourMoukdad88 <m_sy@outlook.com>
2020-02-07 20:55:35 +0200

    Verticals and Horizonals Variables

--- maze.js
+++ maze.js
@@ -29,11 +29,9 @@ World.add(world, walls);
 
 // Maze Generation
 
-const grid = Array(3).fill(null).map(()=> Array(3).fill(false))
-// for(let i = 0; i < 3; i++){
-//   grid.push([]);
-//   for(let j = 0; j < 3; j++) {
-//     grid[i].push(false)
-//   }
-// }
-console.log(grid);
+const grid = Array(3).fill(null).map(()=> Array(3).fill(false));
+const verticals = Array(3).fill(null) .map(()=> Array(2).fill(false));
+const horizontals = Array(2).fill(null).map(()=> Array(3).fill(false));
+console.log(verticals);
+console.log(horizontals);
+
